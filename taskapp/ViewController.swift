72dee2b54c9d4ffa35c3e7e7a4773a075122d025
maedarenfutoshi <japan_lunch_fate@yahoo.co.jp>
2020-07-19 17:51:09 +0900

    relm使って検索条件作ろうと思ったけどよくわからなくなった

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -46,6 +46,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.detailTextLabel?.text = dateString
         // --- ここまで追加 ---
 
+        
+        
+        
         return cell
     }
 
@@ -111,44 +114,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
          tableView.reloadData()
      }
     
-    
-     //MARK: - 渡された文字列を含む要素を検索し、テーブルビューを再表示する
-       func searchItems(searchText: String) {
-           //要素を検索する
-           if searchText != "" {
-               taskArray =  kategory.filter { item in
-                   return  count.contains(searchText)
-               }
-           } else {
-               //渡された文字列が空の場合は全てを表示
-           taskArray =
-
-            //tableViewを再読み込みする
-            tableView.reloadData()
-        }
-        }
-    // MARK: - Search Bar Delegate Methods
-    // テキストが変更される毎に呼ばれる
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        //検索する
-        searchItems(searchText: searchText)
-    }
-
-    // キャンセルボタンが押されると呼ばれる
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.text = ""
-        view.endEditing(true)
-        content.title = task.kategory
-        //tableViewを再読み込みする
-        tableView.reloadData()
-        
-        }
-
-    // Searchボタンが押されると呼ばれる
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        view.endEditing(true)
-        //検索する
-        searchItems(searchText: searchBar.text! as String)
+        _ = realm.objects(Task.self).filter("searchBar.text != ''")
+        taskArray.count 
     }
+    
 }
 
